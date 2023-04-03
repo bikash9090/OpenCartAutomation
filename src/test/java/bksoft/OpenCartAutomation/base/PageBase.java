8dc5536909b3c615b4a5387caefea82900b6e037
bikash <info.bikash9@gmail.com>
2023-04-03 18:21:54 +0530

    Separated common generic actions of page.

--- src/test/java/bksoft/OpenCartAutomation/base/PageBase.java
+++ src/test/java/bksoft/OpenCartAutomation/base/PageBase.java
@@ -1,5 +1,30 @@
 package bksoft.OpenCartAutomation.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import bksoft.OpenCartAutomation.utils.PageActionsUtil;
+
 public class PageBase {
 
+	protected WebDriver driver;
+	PageActionsUtil pageUtil;
+
+	public PageBase(WebDriver driver) {
+		this.driver = driver;
+		pageUtil = new PageActionsUtil(driver);
+	}
+
+	public void click(WebElement element) {
+		element.click();
+	}
+
+	public void flashAndclick(WebElement element) {
+		pageUtil.flash(element);
+		element.click();
+	}
+
+	public void flash(WebElement element) {
+		pageUtil.flash(element);
+	}
 }
