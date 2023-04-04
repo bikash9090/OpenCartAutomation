a0a12cf4df3bc745da4f0849539f6e7a5bd01506
bikash <info.bikash9@gmail.com>
2023-04-04 18:17:25 +0530

    Added flash() , scrollToElement(), moveToElement() common actions.

--- src/test/java/bksoft/OpenCartAutomation/base/PageBase.java
+++ src/test/java/bksoft/OpenCartAutomation/base/PageBase.java
@@ -1,7 +1,9 @@
 package bksoft.OpenCartAutomation.base;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import bksoft.OpenCartAutomation.utils.PageActionsUtil;
 
@@ -9,10 +11,12 @@ public class PageBase {
 
 	protected WebDriver driver;
 	PageActionsUtil pageUtil;
+	Actions action;
 
 	public PageBase(WebDriver driver) {
 		this.driver = driver;
 		pageUtil = new PageActionsUtil(driver);
+		action = new Actions(driver);
 	}
 
 	public void click(WebElement element) {
@@ -20,11 +24,32 @@ public class PageBase {
 	}
 
 	public void flashAndclick(WebElement element) {
-		pageUtil.flash(element);
+		flash(element);
 		element.click();
 	}
 
 	public void flash(WebElement element) {
-		pageUtil.flash(element);
+		JavascriptExecutor js = (JavascriptExecutor) driver; // downcasting
+
+		js.executeScript("arguments[0].setAttribute('style','background: yellow; border: solid 5px red')", element);
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		js.executeScript("arguments[0].setAttribute('style','border: solid 2px white')", element);
+	}
+
+	public void moveToElement(WebElement element) {
+		action.moveToElement(element).build().perform();
+	}
+
+	public void scrolToElement(WebElement element) {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView();", element);
+
 	}
 }
