382e8343720def023ec362fbaf8ac887b72c30a6
bikash <info.bikash9@gmail.com>
2023-04-12 17:11:05 +0530

    Tested the log messages.

--- src/test/java/bksoft/OpenCartAutomation/testcases/HomePageTest.java
+++ src/test/java/bksoft/OpenCartAutomation/testcases/HomePageTest.java
@@ -12,7 +12,7 @@ public class HomePageTest extends TestBase {
 	public void validatingHomepageLocatorsFunctionality() throws InterruptedException {
 		hp = new HomePage(driver);
 
-		 hp.selectCurrency("pound");
+		   hp.selectCurrency("dollar");
 		// hp.clickOnMyAccount();
 		// hp.clickOnRegister();
 		// hp.clickOnLogin();
@@ -23,8 +23,8 @@ public class HomePageTest extends TestBase {
 		// hp.searchItem("iPhone 13");
 		// hp.clickOnCartItems();
 		// hp.clickOnCategory("4");
-		// hp.hoverOverCategory("components");
-		// hp.clickOnSubCategory("printers");
+		 hp.hoverOverCategory("components");
+		 hp.clickOnSubCategory("printers");
 		// hp.clickOnFeaturedProductTitle("canon eos");
 		// hp.getFeaturedProductProductTitle("canon eos");
 		// hp.getFeaturedProductDescription("canon eos");
@@ -32,7 +32,7 @@ public class HomePageTest extends TestBase {
 		// hp.clickOnFeaturedProductAddToCart("iphone");
 		// hp.clickOnFeaturedProductAddToWishList("iphone");
 		// hp.clickOnFeaturedProductCompareThisProduct("macbook");
-		 hp.clickOnHyperLink("extras","brands");
+		// hp.clickOnHyperLink("extras","brands");
 		 
 
 		Thread.sleep(3000);
