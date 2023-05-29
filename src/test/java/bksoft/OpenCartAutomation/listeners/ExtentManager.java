a7171bf526f7829412e7d499925f5d0e9c958490
bikash9090 <info.bikash9@gmail.com>
2023-05-29 18:14:33 +0530

    Added code to auto create folder of and report of extentreport.

--- src/test/java/bksoft/OpenCartAutomation/listeners/ExtentManager.java
+++ src/test/java/bksoft/OpenCartAutomation/listeners/ExtentManager.java
@@ -1,28 +1,53 @@
 package bksoft.OpenCartAutomation.listeners;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import bksoft.OpenCartAutomation.utils.PropertiesUtil;
 
 public class ExtentManager {
+	private static ExtentReports extentReports;
+	private static PropertiesUtil readconfig;
 
-	public static void main(String args[]) {
+	public static ExtentReports init() {
+		readconfig = new PropertiesUtil();
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
+		Path path = Paths.get(readconfig.getReportPath());
+		String reportName = "Extent-Test-Report-" + timeStamp + ".html";
+		// if directory exists?
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectories(path);
+			} catch (IOException e) {
+				// fail to create directory
+				e.printStackTrace();
+			}
+		}
+		
+		extentReports = new ExtentReports();
+		ExtentSparkReporter sparkReporter = new ExtentSparkReporter(readconfig.getReportPath() + reportName);
+		sparkReporter.config().setDocumentTitle("opencart Automation Report"); // Title of report
+		sparkReporter.config().setReportName("opencart Functional Testing"); // name of the report
+		sparkReporter.config().setTheme(Theme.DARK);
+		extentReports.attachReporter(sparkReporter);
+		extentReports.setSystemInfo("Customer Name", "CBT");
+		extentReports.setSystemInfo("Application", "OpenCart");
+		extentReports.setSystemInfo("Module", "User");
+		extentReports.setSystemInfo("Sub Module", "Customers");
+		extentReports.setSystemInfo("Operating System", System.getProperty("os.name"));
+		extentReports.setSystemInfo("User Name", System.getProperty("user.name"));
+		extentReports.setSystemInfo("Environemnt", "QA");
+		extentReports.setSystemInfo("Build#", "1.0");
+		extentReports.setSystemInfo("Team", "OpenCart QA Team");
 
-		ExtentReports extent = new ExtentReports();
-		File path = new File(".\\reports\\reports.html");
-		ExtentSparkReporter sparkReporter = new ExtentSparkReporter(path);
-		extent.attachReporter(sparkReporter);
-
-		extent.createTest("Test1", "Only for testing purpose")
-		.log(Status.FAIL, "This is info 1")
-		.log(Status.INFO, "This is info 2")
-		.log(Status.PASS, "This is info 3")
-		.log(Status.WARNING, "This is info 4")
-		.log(Status.SKIP, "This is info 5")
-		.log(Status.PASS, "This is info 6");
-
-		extent.flush();
+		return extentReports;
 	}
 }
