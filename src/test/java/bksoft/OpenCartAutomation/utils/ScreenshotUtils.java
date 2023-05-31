e0c650c5299bca5738354b305fed019a0b5c77ff
bikash9090 <info.bikash9@gmail.com>
2023-05-31 17:29:47 +0530

    Added new screenshotUtil class.

---
+++ src/test/java/bksoft/OpenCartAutomation/utils/ScreenshotUtils.java
@@ -0,0 +1,36 @@
+package bksoft.OpenCartAutomation.utils;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+
+public class ScreenshotUtils {
+
+	protected WebDriver driver;
+	
+	public ScreenshotUtils(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public String getScreenshot(String testname) {
+		
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir") + "\\screenshots\\" + testname + "_" + timeStamp + ".png";
+		System.out.println("Screenshot captured Successful.");
+
+		try {
+			FileUtils.copyFile(source, new File(destination));
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return destination;
+	}
+}
