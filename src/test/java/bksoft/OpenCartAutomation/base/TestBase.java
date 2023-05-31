c8cebf77263f7522f7ec144f97548d0a5bbef783
bikash9090 <info.bikash9@gmail.com>
2023-05-31 17:30:46 +0530

    Modified code according to singleton design pattern.

--- src/test/java/bksoft/OpenCartAutomation/base/TestBase.java
+++ src/test/java/bksoft/OpenCartAutomation/base/TestBase.java
@@ -1,38 +1,36 @@
 package bksoft.OpenCartAutomation.base;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import bksoft.OpenCartAutomation.utils.PropertiesUtil;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 
-	PropertiesUtil readconfig = new PropertiesUtil();
-	public WebDriver driver;
+	private PropertiesUtil readconfig = new PropertiesUtil();
+	public static WebDriver driver;
 	
-	public void setUpDriver() {
+	public void setUpDriver(){
 		
-		WebDriverManager.chromedriver().setup();
-
-		if (readconfig.getBrowser().equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
-
-		} else if (readconfig.getBrowser().equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
-
-		} else if (readconfig.getBrowser().equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
+		try {
+			driver = DriverFactory.getDriverInstance().getDriver("chrome");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
+		
+		
 		driver.get(readconfig.getUrl());
 		driver.manage().window().maximize();
-		//driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
-	}
+		
+	} 
+
 	
 	public void tearDownDriver() {
-		driver.quit();
+
+		if (driver != null) {	
+			
+			DriverFactory.getDriverInstance().quitDriver();
+		}
 	}
 }
