09c89157ea125a27fbc20a46e3ccc51477a041a8
bikash9090 <info.bikash9@gmail.com>
2023-06-09 17:43:24 +0530

    Added new test cases.

--- src/test/java/bksoft/OpenCartAutomation/testcases/LoginPageTest.java
+++ src/test/java/bksoft/OpenCartAutomation/testcases/LoginPageTest.java
@@ -2,17 +2,18 @@ package bksoft.OpenCartAutomation.testcases;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import bksoft.OpenCartAutomation.base.TestBase;
 import bksoft.OpenCartAutomation.pages.HomePage;
 import bksoft.OpenCartAutomation.pages.LoginPage;
+import bksoft.OpenCartAutomation.utils.ExcelUtils;
 
-public class LoginPageTest extends TestBase{
-	
+public class LoginPageTest extends TestBase {
+
 	HomePage hp;
 	LoginPage login;
-	
 
 	@BeforeClass
 	public void initialization() {
@@ -25,16 +26,18 @@ public class LoginPageTest extends TestBase{
 	public void tearDown() {
 		tearDownDriver();
 	}
-	
-	@Test
-	public void loginToAccountTest() throws InterruptedException {
+
+	@DataProvider
+	private Object[][] LoginCredentials() {
+		return new ExcelUtils().getTestData("LoginCredentials");// Passing the sheet name of Excel file as parameter.
+	}
+
+	@Test(dataProvider = "LoginCredentials", description = "User login form.")
+	public void loginToAccountTest(String uname, String pwd) {
 		hp.clickOnMyAccount();
 		hp.clickOnLogin();
-		login.enterEmail("ABC@gamil.com");
-		login.enterPassword("Pupun&5454");
+		login.enterEmail(uname);
+		login.enterPassword(pwd);
 		login.clickOnLogin();
-		Thread.sleep(5000);
 	}
-	
-
 }
