caa35d58aec2a2a007e96478d28a6039c6e45d6e
bikash9090 <info.bikash9@gmail.com>
2023-06-12 17:46:25 +0530

    Added new testCases and testname , description.

--- src/test/java/bksoft/OpenCartAutomation/testcases/HomePageTest.java
+++ src/test/java/bksoft/OpenCartAutomation/testcases/HomePageTest.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertTrue;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -18,7 +19,7 @@ public class HomePageTest extends TestBase {
 	HomePage hp;
 	ProductsListPage prodlist;
 	Logger log = LogManager.getLogger(HomePageTest.class.getName());
-	
+
 	/*-----------------------------------------------BeforClass initialization-----------------------------------------------------------*/
 	@BeforeClass
 	public void initialization() {
@@ -33,20 +34,20 @@ public class HomePageTest extends TestBase {
 		log.info("Destroying driver object.");
 		tearDownDriver();
 	}
-	
+
 	/*-------------------------------------------------Home page test cases-------------------------------------------------------------*/
-	
+
 	@DataProvider
 	private String[][] currencyData() {
 		return new String[][] { { "Euro", "€" }, { "Pound", "£" }, { "Dollar", "$" } };
 	}
 
-	@Test(testName ="TC-home-008",description = "Verify the currency list functionality.", dataProvider = "currencyData")
+	@Test(testName = "TC-home-008", description = "Verify the currency list functionality.", dataProvider = "currencyData", priority = 1)
 	public void verifyCurrencyList(String currency, String currencySymbol) {
 
 		String product = "macbook";
 
-		log.info("TC_008_select_currency_test.");
+		log.info("TC-home-008");
 		hp.selectCurrency(currency);
 
 		String price = hp.getFeaturedProductPrice(product);
@@ -55,25 +56,51 @@ public class HomePageTest extends TestBase {
 		assertTrue(price.contains(currencySymbol));
 	}
 
-	@Test(testName = "TC-home-009",description = "Verify by clicking on My Account and its sub options.")
+	@Test(testName = "TC-home-009", description = "Verify by clicking on My Account and its sub options.", priority = 2)
 	public void verifyMyAccountList() {
-		
-		log.info("TC-home-009_VerifyMyAccount test.");
+
+		log.info("TC-home-009");
 		hp.clickOnMyAccount();
 		hp.clickOnLogin();
-		
+
 		log.info("Asserting Login page.");
 		SoftAssert softAssert = new SoftAssert();
 		softAssert.assertEquals(hp.getTitle(), "Account 2Login");
-		
+
 		hp.clickOnMyAccount();
 		hp.clickOnRegister();
-		
-		log.info("Asserting Register page.");
+
+		log.info("Soft Asserting verifyMyAccount");
 		softAssert.assertEquals(hp.getTitle(), "Register 2Account");
 		softAssert.assertAll();
 		hp.navigateBack();
+
+	}
+
+	@Test(testName = "TC-home-011", description = "Verify by clicking on logo.", priority = 3)
+	public void clickAndVerifyLogo() {
+
+		log.info("TC-home-011");
+		hp.clickOnLogo();
+
+		log.info("Asserting clicking on logo.");
+		Assert.assertEquals(hp.getTitle(), "Your Store");
+	}
+
+	@DataProvider
+	private String[][] fprodData() {
+		return new String[][] { { "MacBook" }, { "iPhone" }, { "Apple Cinema 30" }, { "Canon EOS 5D" } };
+	}
+
+	@Test(priority = 4, dataProvider = "fprodData", testName = "TC-home-015", description = "Verify by clicking on the image or the titles of featured products.")
+	public void clickAndVerifyFeaturedProducts(String prodName) {
 		
-		
+		log.info("TC-home-015");
+		hp.clickOnFeaturedProductTitle(prodName);
+
+		log.info("Asserting clickAndVerifyFeaturedProducts.");
+		Assert.assertTrue(hp.getTitle().contains(prodName));
+
+		hp.navigateBack();
 	}
 }
